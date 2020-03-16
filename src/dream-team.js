e3318908690a2cf9d7151b4f07ae502b39adc1e1
Vitaliy Skoblin <Vitaliy_Skoblin@epam.com>
2020-03-16 23:26:55 +0400

    feat: add solution for task 'Dream team'

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,19 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if(typeof(members) !== 'object' || !Array.isArray(members)) {
+    return false;
+  }
+  
+  return members.filter((member) => {
+    return typeof member === 'string'
+  })
+  .map((name) => {
+    name = name.toUpperCase();
+
+    for(let i = 0; i < name.length; ++i) {
+      if(name[i] >= 'A' && name[i] <= 'Z') {
+        return name[i];
+      }
+    }
+  })
+  .sort().join('');
 };
