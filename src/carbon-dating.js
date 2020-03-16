acfbd5a99dfb0fe1ce55590f9e985983426e69ae
Vitaliy Skoblin <Vitaliy_Skoblin@epam.com>
2020-03-16 23:11:22 +0400

    feat: add solution for task 'Carbon dating'

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,21 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if(typeof sampleActivity === 'string') {
+    if(!/^[0-9]*[\.]{0,1}[0-9]*[\.]{0,1}[0-9]*$/.test(sampleActivity)) {
+      return false;
+    }
+
+    const activity = parseFloat(sampleActivity.match(/^[0-9]*[\.]{0,1}[0-9]*/));
+
+    if(activity <= 0 || activity >= MODERN_ACTIVITY) {
+      return false;
+    }
+
+    const k = 0.693 / HALF_LIFE_PERIOD;
+    return Math.ceil(Math.log(MODERN_ACTIVITY / activity) / k);
+  }
+
+  return false;
 };
