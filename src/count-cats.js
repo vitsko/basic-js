037610047002236ff18defcefec29291ef30f226
Vitaliy Skoblin <Vitaliy_Skoblin@epam.com>
2020-03-16 22:40:47 +0400

    feat: add solution for task 'Count cats!'

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,11 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
-};
+module.exports = function countCats(matrix) {
+  let catCount = 0;
+
+  for(let i = 0; i < matrix.length; ++i) {
+    for(let j = 0; j < matrix[i].length; ++j) {
+      catCount += (matrix[i][j] === "^^");
+    }
+  }
+
+  return catCount;
+}
