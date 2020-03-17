5c234e42364249f736becff869cee264f2d2dae8
Vitaliy Skoblin <Vitaliy_Skoblin@epam.com>
2020-03-17 22:30:43 +0400

    feat: add solution for task 'Tower of Hanoi'

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,4 +1,9 @@
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+    const SECONDS_PER_HOUR = 3600;
+    var turns = 2 ** disksNumber - 1;
+    
+    return {
+        turns: turns,
+        seconds: turns / (turnsSpeed / SECONDS_PER_HOUR)
+    }
 }
