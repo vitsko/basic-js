ea37988fea35f04a2f836d77b8e7d4a879f90603
Vitaliy Skoblin <Vitaliy_Skoblin@epam.com>
2020-03-17 23:57:43 +0400

    feat: add solution for task 'Vigenere cipher'

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,12 +1,58 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+
+    MOD = 26;
+
+    constructor(direct=true) {
+        this.direct = direct;
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    getSymbolCode(symbol) {
+        return symbol.charCodeAt(0) - 'A'.charCodeAt(0);
+    }
+
+    shift(symbol, length) {
+        const code = this.getSymbolCode(symbol);
+        return String.fromCharCode((code + length) % this.MOD + 'A'.charCodeAt(0));
+    }
+
+    encrypt(string, key) {
+        return this.crypt(string, key, false);
+    }
+
+    decrypt(string, key) {
+        return this.crypt(string, key, true);
+    }
+
+    crypt(string, key, isDecrypt) {
+        if(string === undefined || key == undefined) {
+            throw new Error();
+        }
+
+        string = string.toUpperCase();
+        key = key.toUpperCase();
+
+        let result = [];
+        let coding = 0;
+
+        for(let i = 0; i < string.length; ++i) {
+            if(string[i] >= 'A' && string[i] <= 'Z') {
+                if(isDecrypt) {
+                    result.push(this.shift(string[i], this.MOD - this.getSymbolCode(key[coding % key.length])));
+                } else {
+                    result.push(this.shift(string[i], this.getSymbolCode(key[coding % key.length])));
+                }
+                
+                coding++;
+            } else {
+                result.push(string[i]);
+            }
+        }
+
+        if(!this.direct) {
+            result.reverse();
+        }
+
+        return result.join('');
     }
 }
 
