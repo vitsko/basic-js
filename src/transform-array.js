620d304a93a885bdb4b547ee4fab4867d3d82f35
Vitaliy Skoblin <Vitaliy_Skoblin@epam.com>
2020-03-17 22:49:41 +0400

    feat: add solution for task 'Transform array'

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,39 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    if(typeof(arr) !== 'object') {
+        throw new Error();
+    }
+
+    if(!Array.isArray(arr)) {
+        throw new Error();
+    }
+
+    if(!arr.length) {
+        return arr;
+    }
+
+    const result = [];
+
+    for(let i = 0; i < arr.length; ++i) {
+        if(arr[i] === '--double-next') {
+            if(i < arr.length - 1) {
+                result.push(arr[i + 1]);
+            }
+        } else if(arr[i] === '--double-prev') {
+            if(i > 0) {
+                result.push(arr[i - 1]);
+            }
+        } else if(arr[i] === '--discard-next') {
+            if(i < arr.length - 1) {
+                i++;
+            }
+        } else if(arr[i] === '--discard-prev') {
+            if(result.length > 0) {
+                result.pop();
+            }
+        } else {
+            result.push(arr[i]);
+        }
+    }
+
+    return result;
 };
