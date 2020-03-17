3d2e78ccd7f23bd6e14bff6327652096dc16caa2
Vitaliy Skoblin <Vitaliy_Skoblin@epam.com>
2020-03-17 23:36:19 +0400

    feat: add solution for task 'Extended repeater'

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,41 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    if (options.separator === undefined) {
+        options.separator = '+';
+    }
+
+    if (options.additionSeparator === undefined) {
+        options.additionSeparator = '|';
+    }
+
+    if (options.repeatTimes === undefined) {
+        options.repeatTimes = 1;
+    }
+
+    if (options.additionRepeatTimes === undefined) {
+        options.additionRepeatTimes = 1;
+    }
+
+    if (options.addition === undefined) {
+        options.additionRepeatTimes = 0;
+    }
+
+    let newString = '';
+
+    for (let i = 0; i < options.repeatTimes; ++i) {
+        newString += str;
+
+        for (let j = 0; j < options.additionRepeatTimes; ++j) {
+            newString += options.addition;
+
+            if (j < options.additionRepeatTimes - 1) {
+                newString += options.additionSeparator;
+            }
+        }
+
+        if (i < options.repeatTimes - 1) {
+            newString += options.separator;
+        }
+    }
+
+    return newString;
 };
-  
